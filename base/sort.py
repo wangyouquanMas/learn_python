d1d58e9959c34826d513db0709f89b9044362d41
友权 <wyq1995dr@163.com>
2020-11-23 22:59:58 +0800

    二次提交

--- base/sort.py
+++ base/sort.py
@@ -1,6 +1,24 @@
 #顺序结构
-A = float(input("输入A："))
+A = int(input("输入A："))
 
 print("A=",A)
 
 #判断结构
+if 3<0:
+    print("错误")
+elif 3>0:
+    print("正确")
+
+#循环结构
+i =1
+sum = 0
+while i<A:
+    sum+=i
+    i +=1
+print("%d结果是：%d"%(A,sum))
+
+for i in range(A):
+    sum +=i
+print("%d,%d"%(A,sum))
+
+#异常
